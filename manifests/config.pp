4ae2d8d1ad95ee6f81455ca6e8f9faf6dfb3a21d
nikolafa <nikolafa@ntnu.no>
2018-11-02 16:13:38 +0000

    added stdlib as a dependency and changed so that config generates the config file for vault

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,15 +3,38 @@
 # @summary A short summary of the purpose of this class
 #
 # @example
-#include vault::config
-
+include vault::config
 class vault::config {
 #	$backend 	= { 'file' => { 'path' => '/opt/vault' }}
 #	$listener = { 'tcp' => { 'address' => '127.0.0.1:500',
 #				 'tls_disable' => 1,
 #			},
 #	}
-	
+#
+	$vault_config = {
+		'listener'		=> {
+			'tcp' => {
+				'address' => '127.0.0.1:8200',
+				'tls_disable' => 1,
+			}
+		},
+		'storage'		=> {
+			'file' => {
+				'path' => '/opt/vaultData'
+			}
+		},
+#		'api_addr'		=> 'http://10.212.137.152:8200'
+		'max_lease_ttl'		=> '10h',
+		'default_lease_ttl'	=> '10h',
+		'ui'			=> 'true',
+	}
+	require stdlib
+	file { '/opt/vaultConf.json':
+		ensure	=> present,
+		content	=> to_json($vault_config),
+		owner	=> 'root',
+		group	=> 'root',
+	}	
 	include systemd::systemctl::daemon_reload
 	file { '/etc/systemd/system/vault.service':
 	  ensure 	=> present,
