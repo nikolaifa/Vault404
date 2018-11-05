68698c051e2165c0e80bd83540a8db37f9780e59
root <root@manager.borg.trek>
2018-11-05 14:17:35 +0000

    install pdk validatet

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,40 +6,40 @@
 #include vault::install
 
 class vault::install {
-	file { $::vault::destination_dir:
-		ensure => 'directory',
-		owner  	=> 'vault',
-		group  	=> 'vault',
-		mode   	=> '0640',
-	}
+  file { $::vault::destination_dir:
+    ensure => 'directory',
+    owner  => 'vault',
+    group  => 'vault',
+    mode   => '0640',
+  }
 
-	archive { "${::vault::destination_dir}vault.zip":
-		ensure 		=> present,
-		extract 	=> true,
-		extract_path	=> $::vault::destination_dir,
-		source 		=> $::vault::download_url,
-		creates		=> "${::vault::destination_dir}vault",
-		cleanup		=> true,
-		before		=> File['vault_binary'],
-	}
+  archive { "${::vault::destination_dir}vault.zip":
+    ensure       => present,
+    extract      => true,
+    extract_path => $::vault::destination_dir,
+    source       => $::vault::download_url,
+    creates      => "${::vault::destination_dir}vault",
+    cleanup      => true,
+    before       => File['vault_binary'],
+  }
 
-	file { 'vault_binary':
-		path => "${::vault::destination_dir}vault",
-		owner => 'vault',
-		group => 'vault',
-		mode => '0755',
-	}
-	
-	group { 'vault':
-		ensure => present,
-	}
+  file { 'vault_binary':
+    path  => "${::vault::destination_dir}vault",
+    owner => 'vault',
+    group => 'vault',
+    mode  => '0755',
+  }
 
-	user { 'vault':
-		ensure => 'present',
-		comment => 'For running vault',
-		system => true,
-		home => '/etc/vault.d',
-		shell => '/bin/false',
-	}
+  group { 'vault':
+    ensure => present,
+  }
+
+  user { 'vault':
+    ensure  => 'present',
+    comment => 'For running vault',
+    system  => true,
+    home    => '/etc/vault.d',
+    shell   => '/bin/false',
+  }
 }
 
