c7e1662a89d195228e5eb28e551ef82d6e6cac26
root <root@manager.borg.trek>
2018-11-05 14:41:13 +0000

    config and service files pdk validatet/puppet-lintet

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,39 +6,39 @@
 #include vault::config
 class vault::config {
 
-	file { "${::vault::destination_dir}data":
-		ensure => 'directory',
-			  owner  	=> 'vault',
-			  group  	=> 'vault',
-			  mode   	=> '0640',
-	}
+  file { "${::vault::destination_dir}data":
+    ensure => 'directory',
+      owner=> 'vault',
+      group=> 'vault',
+      mode => '0640',
+  }
 
-	file { "${::vault::destination_dir}vault.json":
-		ensure	=> file,
-		content	=> to_json($::vault::vault_config),
-		
-			  owner  	=> 'vault',
-			  group  	=> 'vault',
-			  mode   	=> '0640',
-	}	
-	
+  file { "${::vault::destination_dir}vault.json":
+    ensure  => file,
+    content => to_json($::vault::vault_config),
 
-	case $::vault::kernel {
-		'linux': {	
-			file { "${::vault::service_path}vault.service":
-			  ensure 	=> present,
-			  owner  	=> 'vault',
-			  group  	=> 'vault',
-			  mode   	=> '0640',
-			  content	=> template('vault/vault.systemd.erb'),
-			}
-			~> Class['systemd::systemctl::daemon_reload']
-		}
-		'windows': {
-		
-		}
-		default: {
-			fail("Unsupported kernel: $::vault::kernel")
-		}
-	}
+      owner => 'vault',
+      group => 'vault',
+      mode  => '0640',
+  }
+
+
+  case $::vault::kernel {
+    'linux': {
+      file { "${::vault::service_path}vault.service":
+        ensure  => present,
+        owner   => 'vault',
+        group   => 'vault',
+        mode    => '0640',
+        content => template('vault/vault.systemd.erb'),
+      }
+      ~> Class['systemd::systemctl::daemon_reload']
+    }
+    'windows': {
+
+    }
+    default: {
+      fail("Unsupported kernel: ${::vault::kernel}")
+    }
+  }
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -6,24 +6,24 @@
 #include vault::service
 
 class vault::service {
-	case $::vault::kernel {
-		'linux': {
-			service { 'vault': 
-				name		=> vault,
-				ensure		=> running,
-				enable		=> true,
-				provider	=> $::vault::provider,
-				subscribe	=> File["${::vault::service_path}vault.service"],
-			}	
-		}
-		'windows': {
-			service { 'vault':
-				name	=> vault,
-				start => "${::vault::destination_dir}vault server -config=${::vault::destination_dir}vault.json",
-				ensure	=> running,
-				enable	=> true,
-			}
-		}
-		default: { fail("Failed to start vault") }
-	}
+  case $::vault::kernel {
+    'linux': {
+      service { 'vault':
+        ensure    => running,
+        name      => vault,
+        enable    => true,
+        provider  => $::vault::provider,
+        subscribe => File["${::vault::service_path}vault.service"],
+      }
+    }
+    'windows': {
+      service { 'vault':
+        ensure => running,
+        start  => "${::vault::destination_dir}vault server -config=${::vault::destination_dir}vault.json",
+        name   => vault,
+        enable => true,
+      }
+    }
+    default: { fail('Failed to start vault') }
+  }
 }
